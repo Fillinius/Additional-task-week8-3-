ef70827f885acf16e536c5a82394b0309011795d
Vladimir Revyagin <skorabeyf@gmail.com>
2023-01-18 11:22:14 +0300

    two-way solutoin

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello');
+// По мне так просто СУПЕР метод
+const unique = (arr) => [...new Set(arr)]
+const result = unique([1, 1, 2, 2, 4, 2, 3, 7, 3])
+console.log(result);
+
+//////////////////////////////////////
+const makeUniq = (arr) => {
+  return arr.filter((el, id) => arr.indexOf(el) === id);
+}
+const resultOther = makeUniq([1, 1, 2, 2, 4, 2, 3, 7, 3]);
+console.log(resultOther);
+//////////////////////////////////////
+
